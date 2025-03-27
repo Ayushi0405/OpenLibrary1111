4ebd8a2a2a7512c3ce4e6cc34eeb1504d61633cc
Ayushi0405 <aayushi.gupta0405@gmail.com>
2025-03-27 23:29:21 +0530

    Deployment Done

--- backend/app.py
+++ backend/app.py
@@ -126,3 +126,7 @@ def get_genres():
 
 if __name__ == "__main__":
     app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve  # Use a production server
+    serve(app, host="0.0.0.0", port=5000)
+
